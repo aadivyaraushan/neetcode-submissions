4f0285d63ecac3bf9ccdfd21429b79fd6a409fc9
Aadivya Raushan <96186374+aadivyaraushan@users.noreply.github.com>
2026-06-28 03:35:56 +0530

    Add: balanced-binary-tree - submission-1

---
+++ Data Structures & Algorithms/balanced-binary-tree/submission-1.py
@@ -0,0 +1,35 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+
+class Solution:
+
+
+    def isBalanced(self, root: Optional[TreeNode]) -> bool:
+        def heightOfSubtree(subtree):
+            if not subtree:
+                return 0
+            return 1 + max(heightOfSubtree(subtree.left), heightOfSubtree(subtree.right))
+        
+        delta = 0
+
+        if not root:
+            return True
+
+        if not root.left:
+            delta = heightOfSubtree(root.right)
+        elif not root.right:
+            delta = heightOfSubtree(root.left)
+        elif not root.left and not root.right:
+            delta = 0
+        else:
+            # invariant: delta accurately represents delta b/w height of left and right subtree
+
+            delta = abs(heightOfSubtree(root.left) - heightOfSubtree(root.right))
+
+            # print(f"for root value: {root.val}, delta b/w {root.left.val} and {root.right.val} is {delta}")
+
+        return delta <= 1 and self.isBalanced(root.left) and self.isBalanced(root.right)
