e2e40e6458390edbea7c31a3102624404108a1cf
Aadivya Raushan <96186374+aadivyaraushan@users.noreply.github.com>
2026-07-06 00:07:15 +0530

    Add: task-scheduling - submission-0

---
+++ Data Structures & Algorithms/task-scheduling/submission-0.py
@@ -0,0 +1,50 @@
+import heapq
+
+class Solution:
+
+    def leastInterval(self, tasks: List[str], n: int) -> int:
+        heap = []
+        queue = []
+
+        freq_map = {}
+        for task in tasks:
+            if task not in freq_map:
+                freq_map[task] = 0
+            freq_map[task] += 1
+        
+        for elem, freq in freq_map.items():
+            heap.append((-freq, elem))
+        
+        heapq.heapify(heap)
+        print(f"heap: {heap}")
+
+        t = 0
+
+        while len(heap) > 0 or len(queue) > 0:
+            t += 1
+            if len(heap) > 0:
+                freq, elem = heapq.heappop(heap)
+                freq = -int(freq)
+                if freq > 1:
+                    queue.append((t + n , -(freq - 1), elem))
+            
+
+            # not sure if this ordering is right
+            # will fix during debugging
+            if len(queue) > 0:
+                if queue[0][0] == t: # gives time
+                    ready_time, freq, elem = queue.pop(0)
+                    if ready_time > 0:
+                        heapq.heappush(heap, (freq, elem))
+            
+        return t
+            
+
+
+
+        
+
+
+
+       
+        
