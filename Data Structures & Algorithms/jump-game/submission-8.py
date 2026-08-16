0e6bdd964f200e2ca6d5ad92204a5043983a72f7
Aadivya Raushan <96186374+aadivyaraushan@users.noreply.github.com>
2026-08-16 00:28:15 -0500

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.py
@@ -0,0 +1,20 @@
+class Solution:
+    def canJump(self, nums: List[int]) -> bool:
+        i = 0
+        while i < len(nums) - 1:
+            if nums[i] == 0:
+                return False
+            maxF = i+1 + nums[i+1]
+            maxj = i+1
+            for j in range(i+1, i+nums[i]+1):
+                if j >= len(nums):
+                    return True
+                F = j + nums[j]
+                if F >= maxF:
+                    maxF = F
+                    maxj = j
+            i = maxj
+        
+
+        # print(f"i: {i}, len(nums) - 1: {len(nums) - 1}")
+        return i >= len(nums) -1 
